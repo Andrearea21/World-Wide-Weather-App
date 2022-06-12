4e9d8d447989a09da9c880f3382652b4d6a1e0e6
Andrearea21 <94383610+Andrearea21@users.noreply.github.com>
2022-06-12 22:31:48 +0200

    current weather information display set up via axios/API
    main function set up for displaying the current weather information.
    Fetching data from open weather map API and inserting the data into HTML page

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
-function showTemperature(response) {
+function showCurrentWeather(response) {
   let temperatureElement = Math.round(response.data.main.temp);
   let currentTemperature = document.querySelector("#temperature");
   let currentCity = document.querySelector("#city");
   let cityElement = response.data.name;
+  let windSpeed = document.querySelector("#wind");
+  let windElement = Math.round(response.data.wind.speed);
+  let currentDescription = document.querySelector("#description-current");
+  let descriptionElement = response.data.weather[0].description;
+  let currentIcon = document.querySelector("#big-icon");
+  let iconElement = response.data.weather[0].icon;
   currentTemperature.innerHTML = `${temperatureElement}`;
   currentCity.innerHTML = `${cityElement}`;
+  windSpeed.innerHTML = `${windElement} `;
+  currentDescription.innerHTML = `${descriptionElement}`;
+  currentIcon.innerHTML = `${currentIcon}`;
+  console.log(response.data.weather[0].icon);
 }
 
 let apiKey = "298f9405a9a634fd43294220b3f6b208";
 let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
-axios.get(apiURL).then(showTemperature);
+axios.get(apiURL).then(showCurrentWeather);
