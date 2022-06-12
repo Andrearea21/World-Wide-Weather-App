2e51c9679dbbca490ffdedf8c74c5f3a5c6e6935
Andrearea21 <94383610+Andrearea21@users.noreply.github.com>
2022-06-12 16:58:17 +0200

    weather search engine  - temperature display
    set up axios script in html file
    got Open Weather Map API and API Key
    set up function to display current temperature
    -City manually in API - search engine integration still missing

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+function showTemperature(response) {
+  let temperatureElement = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector("#temperature");
+  currentTemperature.innerHTML = `${temperatureElement}`;
+}
 
+let apiKey = "298f9405a9a634fd43294220b3f6b208";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiURL).then(showTemperature);
