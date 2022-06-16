3c56ae94f47f58a9fba8fb3d1d1165da90d50052
Andrearea21 <94383610+Andrearea21@users.noreply.github.com>
2022-06-16 10:21:30 +0200

    updated w/new Api key

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row"`;
@@ -46,12 +46,12 @@ function displayForecast() {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "298f9405a9a634fd43294220b3f6b208";
-  let longitude = coordinates.lon;
-  let latitude = coordinates.lat;
-  let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-  axios.get(apiURL).then(displayForecast);
-  console.log(coordinates.lat);
+  let apiKey = "6db953501a1796221633635ef5980630";
+  //let longitude = coordinates.lon;
+  // let latitude = coordinates.lat;
+  let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  //axios.get(apiURL).then(displayForecast);
+  console.log(apiURL);
 }
 
 function showCurrentWeather(response) {
